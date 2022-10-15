f06f78fc8440ae86449d49c34ef5f9fdb8f23462
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2022-10-15 18:18:32 +0200

    :sparkles: finish home page

--- script.js
+++
@@ -1,8 +0,0 @@
-let cursor = document.getElementById('cursor');
-
-window.addEventListener('mousemove', cursorFollowMouse);
-
-function cursorFollowMouse(e){
-  cursor.style.top = e.clientY + 'px';
-  cursor.style.left = e.clientX + 'px';
-}
